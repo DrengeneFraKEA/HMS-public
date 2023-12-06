42c01dc6313d5297435c2227ed954e7b5b93a0d6
Abdi Abdulle <cph-aa394@cphbusiness.dk>
2023-12-06 16:03:12 +0100

    all CRUD logic moved from Controller to Service class #14

--- HMS/Controllers/DrugController.cs
+++ HMS/Controllers/DrugController.cs
@@ -33,28 +33,8 @@ public class DrugController : ControllerBase
     }
 
     [HttpGet("drug_prescriptions")]
-    public IEnumerable<Drug_Prescription> GetDrugsWithPrescriptions()
+    public string GetDrugsWithPrescriptions()
     {
-        var drug_prescriptions = new List<Drug_Prescription>();
-        Database.MySQLContext mysql = new Database.MySQLContext();
-        mysql.Db.Open();
-
-        using var command = new MySqlCommand("SELECT * FROM drug_prescription;", mysql.Db);
-        using var reader = command.ExecuteReader();
-
-        while (reader.Read())
-        {
-            var drug_prescription = new Drug_Prescription()
-            {
-                Name = reader.GetString("name"),
-                Sideeffects = reader.GetString("sideeffects"),
-                Dosage = reader.GetString("dosage"),
-                Instructions = reader.GetString("instructions"),
-            };
-            drug_prescriptions.Add(drug_prescription);
-        }
-        mysql.Db.Close();
-
-        return drug_prescriptions;
+        return drugService.GetDrugsWithPrescriptions();
     }
 }
--- HMS/Services/DrugService.cs
+++ HMS/Services/DrugService.cs
@@ -101,5 +101,30 @@ public class DrugService
 
         return JsonSerializer.Serialize(drugs);
     }
+
+    public string GetDrugsWithPrescriptions()
+    {
+        var drug_prescriptions = new List<Drug_Prescription>();
+        Database.MySQLContext mysql = new Database.MySQLContext();
+        mysql.Db.Open();
+
+        using var command = new MySqlCommand("SELECT * FROM drug_prescription;", mysql.Db);
+        using var reader = command.ExecuteReader();
+
+        while (reader.Read())
+        {
+            var drug_prescription = new Drug_Prescription()
+            {
+                Name = reader.GetString("name"),
+                Sideeffects = reader.GetString("sideeffects"),
+                Dosage = reader.GetString("dosage"),
+                Instructions = reader.GetString("instructions"),
+            };
+            drug_prescriptions.Add(drug_prescription);
+        }
+        mysql.Db.Close();
+
+        return JsonSerializer.Serialize(drug_prescriptions);
+    }
 }
 
